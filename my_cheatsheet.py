81fe9eb748687fd144ca8fc83276c0ec37d7332b
Sajid Fadlelseed <s.q.fadlelseed@herts.ac.uk>
2024-07-21 11:05:15 +0000

    Adds

---
+++ my_cheatsheet.py
@@ -0,0 +1,13 @@
+
+# The Following Usages are not a standard use for numpy slicing, 
+# but they make it easier to target specific ROWS and COLUMNS based on their INDEX.
+
+#2D
+    # arr2d[::, COLUMN_INDEX]
+#3D 
+    # arr3d[::, ROW_INDEX, COLUMN_INDEX]
+    # arr3d[::, ::, COLUMN_INDEX]
+
+
+
+
